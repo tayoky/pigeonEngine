e56260c8c02b21ec95c98dfacb00af4ecb6d129e
tayoky <139649656+tayoky@users.noreply.github.com>
2024-03-01 11:03:35 +0100

    add perspectuve

--- PigeonEgnine.js
+++ PigeonEgnine.js
@@ -1,4 +1,17 @@
+function Calcul(){
+    //initialistion
+    var CalculVertex;
+    CalculVertex=Vertex;
 
+    //perspective
+    PlanVertex = [];
+    for(var Ver in CalculVertex){
+        PlanVertex.push([
+            Ver[0] * field / Ver[2],
+            Ver[1] * field / Ver[2]
+        ]);
+    }
+}
 
 function AddPrimitive(name,x,y,z,size){
     if(name=="cube"){
@@ -16,4 +29,14 @@ function AddPrimitive(name,x,y,z,size){
 
     }
 }
-let Vertex=[];
+//input
+var FOV =60;
+var field ;
+var Vertex=[]; //3Dvertex
+
+
+var PlanVertex=[]//2D vertex
+var canvas =document.getElementById("3D");
+
+
+
